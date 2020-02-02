0a2e49d14de812153da78ff758ca1bf78388617f
rvnovaes <rvnovaes@gmail.com>
2020-02-01 21:15:57 -0300

    Detailed user fields

--- web/authentication/admin.py
+++ web/authentication/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email',
+                    'is_staff', 'is_active', 'is_superuser', 'is_analyst', 'is_manager', 'is_administrator')
+    list_display_links = ('username',)
+    list_editable = ('is_staff', 'is_active', 'is_superuser', 'is_analyst', 'is_manager', 'is_administrator')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'is_analyst', 'is_manager', 'is_administrator')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'is_analyst', 'is_manager', 'is_administrator',
+            'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+
+admin.site.register(User, CustomUserAdmin)
--- web/authentication/models.py
+++ web/authentication/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_analyst = models.BooleanField(
+        _('Analyst'),
+        default=False,
+        help_text=_(
+            'Analyst is the lowest level on hierarchy of an organization.'))
+    is_manager = models.BooleanField(
+        _('Manager'),
+        default=False,
+        help_text=_(
+            'A manager is in charge of a business unit.'
+        ))
+    is_administrator = models.BooleanField(
+        _('Administrator'),
+        default=False,
+        help_text=_(
+            'Administrator of the organization. Not to be confused with a Superuser.'
+        ))
