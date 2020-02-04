f1fd292b95f103cc2afe3316038142f2039dd8a0
rvnovaes <rvnovaes@gmail.com>
2020-02-03 21:34:49 -0300

    Minor correction on user form

--- web/authentication/forms.py
+++ web/authentication/forms.py
@@ -4,6 +4,7 @@ from .models import User
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
-    class Meta():
+
+    class Meta:
         model = User
         fields = ('username', 'password')
