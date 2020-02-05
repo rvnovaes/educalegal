fcd615c79cb789acb35f998122b457aceb5dd250
Roberto Novaes <rvnovaes@gmail.com>
2020-02-05 10:39:59 -0300

    Basic CRUD of company ready

--- web/person/urls.py
+++ web/person/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import CompanyCreate, CompanyUpdate, CompanyDetailView
+from .views import CompanyCreateView, CompanyUpdateView, CompanyDetailView, CompanyListView, CompanyDeleteView
 
 app_name = 'person'
 
 
 urlpatterns = [
+    path('company/', CompanyListView.as_view(), name='company-list'),
     path('company/<int:pk>', CompanyDetailView.as_view(), name='company-detail'),
-    path('company/add/', CompanyCreate.as_view(), name='company-add'),
-    # path('company/<int:pk>/', CompanyUpdate.as_view(), name='company-update'),
-
-    # path('company/<int:pk>/delete/', CompanyDelete.as_view(), name='company-delete'),
+    path('company/create/', CompanyCreateView.as_view(), name='company-create'),
+    path('company/update/<int:pk>/', CompanyUpdateView.as_view(), name='company-update'),
+    path('company/delete/<int:pk>', CompanyDeleteView.as_view(), name='company-delete'),
 ]
--- web/person/views.py
+++ web/person/views.py
@@ -1,31 +1,39 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from audit.mixins import AuditFormMixin
-
 
 
 from .models import Company
 
 
-class CompanyDetailView(DetailView):
+class CompanyDetailView(LoginRequiredMixin, DetailView):
     model = Company
 
 
-class CompanyCreate(AuditFormMixin, CreateView):
-    model = Company
-    fields = ['name', 'legal_name']
-
-
-class CompanyUpdate(UpdateView):
+class CompanyCreateView(AuditFormMixin, CreateView):
     model = Company
     fields = ['name', 'legal_name']
 
 
-# class CompanyDelete(DeleteView):
-#     model = Company
-#     fields = ['name', 'legal_name']
+class CompanyUpdateView(AuditFormMixin, UpdateView):
+    model = Company
+    fields = ['name', 'legal_name']
+
+
+class CompanyListView(LoginRequiredMixin, ListView):
+    model = Company
+    context_object_name = 'companies'
+
+
+class CompanyDeleteView(DeleteView):
+    model = Company
+    success_url = reverse_lazy('person:company-list')
 
 
