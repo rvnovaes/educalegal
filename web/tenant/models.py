69a9ec9a540666f5bf04506758dae24742b997e2
rvnovaes <rvnovaes@gmail.com>
2020-02-06 22:07:01 -0300

    Minor fixes on tenant and linting

--- web/tenant/models.py
+++ web/tenant/models.py
@@ -5,6 +5,9 @@ class Tenant(models.Model):
     name = models.CharField(max_length=100)
     subdomain_prefix = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class TenantAwareModel(models.Model):
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, default=1)
