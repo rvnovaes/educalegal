2e9625a99c52a56d19198aa7b5fc391d1dac3d9e
rvnovaes <rvnovaes@gmail.com>
2020-02-08 08:22:29 -0300

    Created view tenant mixins

--- web/school/views.py
+++ web/school/views.py
@@ -1,58 +1,41 @@
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django_tables2 import SingleTableView
 
+from tenant.mixin import TenantAwareViewMixin, TenantAwareCreateUpdateMixin
 from .models import School
 from .tables import SchoolTable
 
 
-class SchoolCreateView(LoginRequiredMixin, CreateView):
+class SchoolCreateView(LoginRequiredMixin, TenantAwareCreateUpdateMixin, CreateView):
     model = School
     fields = ['name', 'legal_name', 'legal_type', 'cnpj', 'logo', 'street',
               'street_number', 'unit', 'city_region', 'zip_code', 'phone', 'site',
               'email', 'city', 'state', 'country']
 
-    def form_valid(self, form):
-        form.instance.tenant_id = self.request.user.tenant_id
-        return super().form_valid(form)
 
-
-class SchoolDeleteView(LoginRequiredMixin, DeleteView):
+class SchoolDeleteView(LoginRequiredMixin, TenantAwareViewMixin, DeleteView):
     model = School
     context_object_name = 'school'
     success_url = reverse_lazy('school:school-list')
 
-    def get_queryset(self):
-        return School.objects.filter(tenant_id=self.request.user.tenant_id)
 
-
-class SchoolDetailView(LoginRequiredMixin, DetailView):
+class SchoolDetailView(LoginRequiredMixin, TenantAwareViewMixin, DetailView):
     model = School
     context_object_name = 'school'
 
-    def get_queryset(self):
-        return School.objects.filter(tenant_id=self.request.user.tenant_id)
 
-
-class SchoolListView(LoginRequiredMixin, SingleTableView):
+class SchoolListView(LoginRequiredMixin, TenantAwareViewMixin, SingleTableView):
     model = School
     table_class = SchoolTable
     context_object_name = 'schools'
 
-    def get_queryset(self):
-        return School.objects.filter(tenant_id=self.request.user.tenant_id)
 
-
-class SchoolUpdateView(LoginRequiredMixin, UpdateView):
+class SchoolUpdateView(LoginRequiredMixin, TenantAwareCreateUpdateMixin, UpdateView):
     model = School
     fields = ['name', 'legal_name', 'legal_type', 'cnpj', 'logo', 'street',
               'street_number', 'unit', 'city_region', 'zip_code', 'phone', 'site',
               'email', 'city', 'state', 'country']
-
-    def form_valid(self, form):
-        form.instance.tenant_id = self.request.user.tenant_id
-        return super().form_valid(form)
-
---
+++ web/tenant/mixin.py
@@ -0,0 +1,11 @@
+class TenantAwareViewMixin:
+
+    def get_queryset(self):
+        return self.model.objects.filter(tenant_id=self.request.user.tenant_id)
+
+
+class TenantAwareCreateUpdateMixin:
+
+    def form_valid(self, form):
+        form.instance.tenant_id = self.request.user.tenant_id
+        return super().form_valid(form)
