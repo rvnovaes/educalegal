c74c50f17f023f70cafbdbdff5666868336a6b98
rvnovaes <rvnovaes@gmail.com>
2020-02-09 22:10:22 -0300

    Ading methods for generic interviews

--- web/interview/models.py
+++ web/interview/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-from tenant.models import TenantAwareModel
+from tenant.models import Tenant
 
 
-class Interview(TenantAwareModel):
+class Interview(models.Model):
     name = models.CharField(max_length=512, null=True)
     version = models.CharField(max_length=512, null=True)
     date_available = models.DateField(null=True)
     description = models.TextField(null=True)
-    url = models.URLField(max_length=512)
+    base_url = models.URLField(max_length=512)
+    tenants = models.ManyToManyField(Tenant, related_name='tenants')
 
     def __str__(self):
         return self.name
--- web/interview/views.py
+++ web/interview/views.py
@@ -9,4 +9,12 @@ class InterviewListView(LoginRequiredMixin, ListView):
     context_object_name = "interviews"
 
     def get_queryset(self):
-        return Interview.objects.filter(tenant_id=self.request.user.tenant_id)
+        return Interview.objects.filter(tenants=self.request.user.tenant_id)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        uuid = self.request.user.tenant.unique_id
+        for item in context['object_list']:
+            item.interview_url_with_uuid = item.base_url + '&tid=' + str(uuid) + '&new_session=1'
+            print(item.interview_url_with_uuid)
+        return context
