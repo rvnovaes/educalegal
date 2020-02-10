67b6fca2d3ef2e8a581c9cf7c62edf7102354fac
rvnovaes <rvnovaes@gmail.com>
2020-02-10 12:00:56 -0300

    Changed docassemble files to use id instead of uuid

--- api_requests/element_educalegal_app.py
+++ api_requests/element_educalegal_app.py
@@ -1,5 +1,6 @@
 import coreapi
 
+
 def get_tenant_data(schema, params):
     # Initialize a client & load the schema document
     client = coreapi.Client()
@@ -10,3 +11,23 @@ def get_tenant_data(schema, params):
     result = client.action(schema, action, params=params)
     # Return list of tenants. As we consulted by uuid, it will always return only one
     return result[0]
+
+
+def get_school_data(schema, params):
+    # Initialize a client & load the schema document
+    client = coreapi.Client()
+    schema = client.get(schema)
+
+    # Interact with the API endpoint
+    action = ["school", "read"]
+    result = client.action(schema, action, params=params)
+    # Return list of tenants. As we consulted by uuid, it will always return only one
+    return result[0]
+
+
+if __name__ == "__main__":
+    schema = "http://localhost:8000/api/docs"
+    params = {"id": 1}
+    result = get_tenant_data(schema, params)
+    # result = get_school_data(schema, params)
+    print(result)
