6d0cf9015ebd495d3bffa114752df24c676f4070
rvnovaes <rvnovaes@gmail.com>
2020-02-22 19:09:43 -0300

    Improved and corrected interview_dev.yaml

--- web/interview/admin.py
+++ web/interview/admin.py
@@ -5,13 +5,17 @@ from .models import Interview, InterviewDocumentType
 
 @admin.register(Interview)
 class InterviewAdmin(admin.ModelAdmin):
-    search_fields = ['name']
-    list_display = ['name', "version", "date_available", 'list_tenants']
-    filter_horizontal = ['tenants']
+    search_fields = ["name"]
+    list_display = [
+        "name",
+        "version",
+        "document_type",
+        "date_available",
+        "list_tenants",
+    ]
+    filter_horizontal = ["tenants"]
 
 
+@admin.register(InterviewDocumentType)
 class InterviewDocumentTypeAdmin(admin.ModelAdmin):
     pass
-
-
-admin.site.register(InterviewDocumentType, InterviewDocumentTypeAdmin)
