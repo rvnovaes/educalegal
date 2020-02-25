11d88c286e8463ab6c2266bf770eee099eb5307f
rvnovaes <rvnovaes@gmail.com>
2020-02-25 16:52:31 -0300

    Saving Mayan data into Educa Legal API

--- api_requests/mayan_requests.py
+++ api_requests/mayan_requests.py
@@ -1,56 +1,46 @@
-import requests
-from requests.adapters import HTTPAdapter
-from urllib3.util import Retry
-
-# https://www.peterbe.com/plog/best-practice-with-retries-with-requests
-def requests_retry_session(
-    retries=3,
-    backoff_factor=0.3,
-    status_forcelist=(500, 502, 504, 404),
-    session=None,
-):
-    session = session or requests.Session()
-    retry = Retry(
-        total=retries,
-        read=retries,
-        connect=retries,
-        backoff_factor=backoff_factor,
-        status_forcelist=status_forcelist,
-    )
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    return session
+from requests import Session
+# https://github.com/bustawin/retry-requests
+from retry_requests import retry
 
 
-def document_create(url, headers, filename, document_type, label='', language='', description=''):
-    file_object = open(filename, mode='rb')
-    payload = {'document_type': document_type,
-               'label': label,
-               'language': language,
-               'description': description
-    }
-    response = requests.post(url, headers=headers, data=payload, files={"file": file_object})
-    return response
+class MayanClient:
+    def __init__(self, api_base_url, token):
+        self.api_base_url = api_base_url
+        headers = {"Authorization": "Token " + token}
+        self.session = retry(
+            Session(),
+            retries=3,
+            backoff_factor=0.5,
+            status_to_retry=(500, 502, 504, 404),
+        )
+        self.session.headers.update(headers)
 
-def document_read(url, headers):
-    # response = requests.get(url, headers=headers)
-    # return response
-    response = requests_retry_session().get(url, headers=headers)
-    return response
+    def document_create(self, filename, document_type, label='', language='', description=''):
+        file_object = open(filename, mode='rb')
+        payload = {'document_type': document_type,
+                   'label': label,
+                   'language': language,
+                   'description': description
+        }
+        final_url = self.api_base_url + "/api/documents/"
+        response = self.session.post(final_url, data=payload, files={"file": file_object})
+        return response
+
+    def document_read(self, document_id):
+        final_url = self.api_base_url + "/api/documents/{id}".format(id=document_id)
+        response = self.session.get(final_url).json()
+        return response
+
 
 if __name__ == '__main__':
-    url = 'http://localhost:8080/api/documents/'
+    base_url = 'http://localhost:8080'
     tenant_ged_token = 'e12eafe1a884fcce1613f294ac66180efe1b6337'
-    token_string = 'Token ' + tenant_ged_token
-    headers = {'Authorization': token_string}
-    # filename = 'lorem-ipsum.pdf'
-    # label = 'lorem_ipsum_teste'
-    # response = document_create(url, headers, filename, 2, label, 'por', 'Apenas um teste...' ).json()
-    # print(response)
-    # document_id =
+    mc = MayanClient(base_url, tenant_ged_token)
+    filename = 'lorem-ipsum.pdf'
+    label = 'lorem_ipsum_teste'
+    response = mc.document_create(filename, 2, label, 'por', 'Apenas um teste...').json()
+    print(response)
     document_id = 21
-    url = 'http://localhost:8080/api/documents/{id}/'.format(id=document_id)
-    response = document_read(url, headers)
+    response = mc.document_read(document_id)
     print(response)
 
--- web/document/models.py
+++ web/document/models.py
@@ -5,20 +5,36 @@ from school.models import School
 
 
 class Document(TenantAwareModel):
-    name = models.CharField(max_length=512, verbose_name="Nome")
-    school = models.ForeignKey(School, on_delete=models.CASCADE, verbose_name="Escola")
-    created_date = models.DateTimeField(
-        auto_now_add=True, verbose_name="Criação"
-    )
+    name = models.CharField(max_length=512, null=True, verbose_name="Nome")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Criação")
     altered_date = models.DateTimeField(auto_now=True, verbose_name="Alteração")
-    interview = models.ForeignKey(
-        Interview, on_delete=models.CASCADE, verbose_name="Modelo"
+    status = models.CharField(
+        max_length=256, null=True, blank=True, verbose_name="Status"
     )
-    status = models.CharField(max_length=256, verbose_name="Status")
-    signing_provider = models.CharField(max_length=256, verbose_name="Provedor")
-    ged_link = models.URLField(null=True, blank=True, verbose_name="Link")
-    description = models.TextField(null=True, verbose_name='Descrição')
-    related_documents = models.ManyToManyField('self', related_name='documents')
+    envelope_id = models.CharField(
+        max_length=256, null=True, blank=True, verbose_name="Id do Envelope"
+    )
+    signing_provider = models.CharField(
+        max_length=256, null=True, blank=True, verbose_name="Provedor"
+    )
+    ged_id = models.CharField(max_length=128, null=True, blank=True, verbose_name='ID do Documento no GED')
+    ged_link = models.CharField(max_length=256, null=True, blank=True, verbose_name="Link")
+    ged_uuid = models.CharField(
+        max_length=256,
+        null=True,
+        help_text="UUID do documento. UUID = Universally Unique ID.",
+        verbose_name="UUID",
+    )
+    description = models.TextField(null=True, blank=True, verbose_name="Descrição")
+    interview = models.ForeignKey(
+        Interview, null=True, on_delete=models.CASCADE, verbose_name="Modelo"
+    )
+    school = models.ForeignKey(
+        School, null=True, on_delete=models.CASCADE, verbose_name="Escola"
+    )
+    related_documents = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="documents"
+    )
 
     def __str__(self):
-        return self.name + ' - ' + self.school.name
+        return self.name + " - " + self.school.name
