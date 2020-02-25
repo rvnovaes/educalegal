86f1f05ed23c690d955ed2428549f5607d476d71
rvnovaes <rvnovaes@gmail.com>
2020-02-25 10:54:08 -0300

    Changed requests to educalegal api to requests instead of coreapi. Implemented retry requests

--- api_requests/educalegal_coreapi.py
+++ api_requests/educalegal_coreapi.py
@@ -12,24 +12,24 @@ if __name__ == "__main__":
     ut = "32c7ff84169e1aa7f33e1dc23abb72b394fc53dc"
     schema = "http://localhost:8000/v1/docs"
     el_client = EducaLegalClient(ut, schema)
-    # try:
-    #     school_names_list, school_data_dict = el_client.get_all_schools_names_data(1)
-    #     selected_school = "Escola da Lagoa"
-    #     print(school_names_list)
-    #     print(school_data_dict)
-    #     for school_name in school_names_list:
-    #         print(school_name)
-    #     for school_data in school_data_dict:
-    #         try:
-    #             selected_school_data = school_data_dict[selected_school]
-    #             print(selected_school_data)
-    #         except KeyError:
-    #             pass
-    # except ErrorMessage as e:
-    #     print(e)
-    # print(el_client.get_tenant_ged_data(1))
-    # print(el_client.get_interview_data(2))
-    # print(el_client.get_tenant_esignature_data(2))
+    try:
+        school_names_list, school_data_dict = el_client.get_all_schools_names_data(2)
+        selected_school = "Escola da Lagoa"
+        print(school_names_list)
+        print(school_data_dict)
+        for school_name in school_names_list:
+            print(school_name)
+        for school_data in school_data_dict:
+            try:
+                selected_school_data = school_data_dict[selected_school]
+                print(selected_school_data)
+            except KeyError:
+                pass
+    except ErrorMessage as e:
+        print(e)
+    print(el_client.get_tenant_ged_data(1))
+    print(el_client.get_interview_data(2))
+    print(el_client.get_tenant_esignature_data(2))
     print(el_client.create_document(
         "20200255_033504_contrato_de_prestacao_de_servicos_educacionais.pdf",
         "sent",
--- api_requests/educalegal_requests.py
+++ api_requests/educalegal_requests.py
@@ -1,19 +1,107 @@
-import requests
+from requests import Session
+#https://github.com/bustawin/retry-requests
+from retry_requests import retry
 
 
-token = "311648a5e598e7d60b3e7f982909a34a0214f1f4"
-token_string = "Token " + token
-headers = {"Authorization": token_string}
+class EducaLegalClient:
 
+    def __init__(self, api_base_url, token):
+        self.api_base_url = api_base_url
+        headers = {"Authorization": "Token " + token}
+        self.session = retry(Session(), retries=3, backoff_factor=0.5, status_to_retry=(500, 502, 504, 404))
+        self.session.headers.update(headers)
 
-def get_docusign_config_from_educa_legal(tid):
-    esignature_url = "http://localhost:8000/v1/tenants/{tid}/esignature/".format(
-        tid=tid
-    )
-    response = requests.get(esignature_url, headers=headers).json()
-    return response
+    def tenants_ged_read(self, tid):
+        final_url = self.api_base_url + "/v1/tenants/{id}/ged/".format(id=tid)
+        response = self.session.get(final_url).json()
+        return response
+
+    def tenants_esignature_read(self, tid):
+        final_url = self.api_base_url + "/v1/tenants/{id}/esignature/".format(id=tid)
+        response = self.session.get(final_url).json()
+        return response
+
+    def tenants_schools_list(self, tid):
+        final_url = self.api_base_url + "/v1/tenants/{id}/schools/".format(id=tid)
+        response = self.session.get(final_url).json()
+        return response
+
+    def tenants_schools_names(self, tid):
+        schools_list = self.tenants_schools_list(tid)
+        school_names_list = list()
+        for school in schools_list:
+            school_names_list.append(school["name"])
+        return school_names_list
+
+    def tenants_school_names_school_data(self, tid):
+        schools_list = self.tenants_schools_list(tid)
+        school_names_list = list()
+        school_data_dict = dict()
+        for school in schools_list:
+            school_names_list.append(school["name"])
+            school_data_dict[school["name"]] = school
+        return school_names_list, school_data_dict
+
+    def interviews_read(self, intid):
+        final_url = self.api_base_url + "/v1/interviews/{id}".format(id=intid)
+        response = self.session.get(final_url).json()
+        return response
+
+    def create_document(
+            self,
+            name,
+            status,
+            envelope_id,
+            signing_provider,
+            ged_link,
+            ged_uuid,
+            description,
+            tenant,
+            school,
+            interview,
+            related_documents,
+    ):
+        payload = {
+            "name": name,
+            "status": status,
+            "envelope_id": envelope_id,
+            "signing_provider": signing_provider,
+            "ged_link": ged_link,
+            "ged_uuid": ged_uuid,
+            "description": description,
+            "tenant": tenant,
+            "school": school,
+            "interview": interview,
+            "related_documents": related_documents,
+        }
+        final_url = self.api_base_url + "/v1/documents/"
+        response = self.session.post(final_url, data=payload)
+        return response
+
 
 
 if __name__ == "__main__":
-    response = get_docusign_config_from_educa_legal(2)
-    print(response)
+    api_base_url = 'http://localhost:8000'
+    token = "311648a5e598e7d60b3e7f982909a34a0214f1f4"
+    el = EducaLegalClient(api_base_url, token)
+    print(el.tenants_ged_read(2))
+    print(el.tenants_esignature_read(2))
+    print(el.tenants_schools_list(2))
+    print(el.tenants_schools_names(2))
+    print(el.tenants_school_names_school_data(2))
+    print(el.interviews_read(2))
+    print(el.create_document(
+        "20200255_033504_contrato_de_prestacao_de_servicos_educacionais.pdf",
+        "sent",
+        "035322ff-6acc-4c1d-992d-a6a68ca6b68a",
+        "Docusign",
+        "Empty",
+        "179dad8b-99b0-494a-8f81-257d3705029d ",
+        "Contrato de prestação de serviços educacionais entre aluno e escola. - 1.1 - 2020-01-23",
+        2,
+        1,
+        2,
+        None,))
+
+
+
