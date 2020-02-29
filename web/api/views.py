f89692ad568c1d35c484abbf9959300aad9bdd45
rvnovaes <rvnovaes@gmail.com>
2020-02-29 19:36:31 -0300

    Corrected saved log data

--- web/api/views.py
+++ web/api/views.py
@@ -33,76 +33,76 @@ logger = logging.getLogger(__name__)
 
 def docusign_xml_parser(data):
     envelope_data = dict()
-    xml = xmltodict.parse(data)['DocuSignEnvelopeInformation']
-    envelope_data['envelope_id'] = xml['EnvelopeStatus']['EnvelopeID']
-    envelope_data['envelope_status'] = xml['EnvelopeStatus']['Status']
-    envelope_data['envelope_created'] = xml['EnvelopeStatus']['Created']
-    envelope_data['envelope_sent'] = xml['EnvelopeStatus']['Sent']
-    envelope_data['envelope_time_generated'] = xml['EnvelopeStatus']['TimeGenerated']
+    xml = xmltodict.parse(data)["DocuSignEnvelopeInformation"]
+    envelope_data["envelope_id"] = xml["EnvelopeStatus"]["EnvelopeID"]
+    envelope_data["envelope_status"] = xml["EnvelopeStatus"]["Status"]
+    envelope_data["envelope_created"] = xml["EnvelopeStatus"]["Created"]
+    envelope_data["envelope_sent"] = xml["EnvelopeStatus"]["Sent"]
+    envelope_data["envelope_time_generated"] = xml["EnvelopeStatus"]["TimeGenerated"]
 
     e_status_detail = (
-            "Envelope ID: "
-            + envelope_data['envelope_id']
-            + "\n"
-            + "Envelope Status: "
-            + envelope_data['envelope_status']
-            + "\n"
-            + "Envelope Created: "
-            + envelope_data['envelope_created']
-            + "\n"
-            + "Envelope Sent: "
-            + envelope_data['envelope_sent']
-            + "\n"
-            + "Time Generated: "
-            + envelope_data['envelope_time_generated']
-            + "\n"
+        "Envelope ID: "
+        + envelope_data["envelope_id"]
+        + "\n"
+        + "Envelope Status: "
+        + envelope_data["envelope_status"]
+        + "\n"
+        + "Envelope Created: "
+        + envelope_data["envelope_created"]
+        + "\n"
+        + "Envelope Sent: "
+        + envelope_data["envelope_sent"]
+        + "\n"
+        + "Time Generated: "
+        + envelope_data["envelope_time_generated"]
+        + "\n"
     )
-    envelope_data['envelope_status_detail_message'] = e_status_detail
-    recipient_statuses = xml['EnvelopeStatus']['RecipientStatuses']['RecipientStatus']
+    envelope_data["envelope_status_detail_message"] = e_status_detail
+    recipient_statuses = xml["EnvelopeStatus"]["RecipientStatuses"]["RecipientStatus"]
 
     r_status_detail = ""
     for r in recipient_statuses:
         r_status_detail += (
-                r['RoutingOrder']
-                + " - "
-                + r['UserName']
-                + " - "
-                + r['Email']
-                + " - "
-                + r['Type']
-                + " - "
-                + r['Status']
-                + "\n"
+            r["RoutingOrder"]
+            + " - "
+            + r["UserName"]
+            + " - "
+            + r["Email"]
+            + " - "
+            + r["Type"]
+            + " - "
+            + r["Status"]
+            + "\n"
         )
-    envelope_data['envelope_recipient_status_detail_message'] = r_status_detail
+    envelope_data["envelope_recipient_status_detail_message"] = r_status_detail
     all_details = (
-            "### Detalhes do Envelope ###\n"
-            + e_status_detail
-            + "\n"
-            + "### Detalhes dos Destinatários ###\n"
-            + r_status_detail
-            + "\n"
+        "### Detalhes do Envelope ###\n"
+        + e_status_detail
+        + "\n"
+        + "### Detalhes dos Destinatários ###\n"
+        + r_status_detail
+        + "\n"
     )
-    envelope_data['envelope_all_details_message'] = all_details
+    envelope_data["envelope_all_details_message"] = all_details
     return envelope_data
 
 
 def docusign_pdf_files_saver(data, envelope_dir):
     pdf_documents = list()
-    xml = xmltodict.parse(data)['DocuSignEnvelopeInformation']
+    xml = xmltodict.parse(data)["DocuSignEnvelopeInformation"]
     # Loop through the DocumentPDFs element, storing each document.
-    for pdf in xml['DocumentPDFs']['DocumentPDF']:
-        if pdf['DocumentType'] == "CONTENT":
-            filename = "Completed_" + pdf['Name']
-        elif pdf['DocumentType'] == "SUMMARY":
-            filename = pdf['Name']
+    for pdf in xml["DocumentPDFs"]["DocumentPDF"]:
+        if pdf["DocumentType"] == "CONTENT":
+            filename = "Completed_" + pdf["Name"]
+        elif pdf["DocumentType"] == "SUMMARY":
+            filename = pdf["Name"]
         else:
-            filename = pdf['DocumentType'] + "_" + pdf['Name']
+            filename = pdf["DocumentType"] + "_" + pdf["Name"]
         pdf_documents.append(filename)
 
         full_filename = os.path.join(envelope_dir, filename)
         with open(full_filename, "wb") as pdf_file:
-            pdf_file.write(base64.b64decode(pdf['PDFBytes']))
+            pdf_file.write(base64.b64decode(pdf["PDFBytes"]))
     return pdf_documents
 
 
@@ -113,16 +113,20 @@ def docusign_webhook_listener(request):
     logger.info(request.content_type)
     data = request.body  # This is the entire incoming POST content in Django
     try:
-        envelope_data = docusign_xml_parser(data) # Parses XML data and returns a dictionary and formated messages
-        logger.info(envelope_data['envelope_id'])
-        logger.info(envelope_data['envelope_time_generated'])
-        logger.debug(envelope_data['envelope_all_details_message'])
+        envelope_data = docusign_xml_parser(
+            data
+        )  # Parses XML data and returns a dictionary and formated messages
+        logger.info(envelope_data["envelope_id"])
+        logger.info(envelope_data["envelope_time_generated"])
+        logger.debug(envelope_data["envelope_all_details_message"])
 
         # Store the XML file on disk
-        envelope_dir = os.path.join(BASE_DIR, "media/docusign/", envelope_data['envelope_id'])
+        envelope_dir = os.path.join(
+            BASE_DIR, "media/docusign/", envelope_data["envelope_id"]
+        )
         Path(envelope_dir).mkdir(parents=True, exist_ok=True)
         filename = (
-                envelope_data['envelope_time_generated'].replace(":", "_") + ".xml"
+            envelope_data["envelope_time_generated"].replace(":", "_") + ".xml"
         )  # substitute _ for : for windows-land
         filepath = os.path.join(envelope_dir, filename)
         with open(filepath, "wb") as xml_file:
@@ -134,9 +138,11 @@ def docusign_webhook_listener(request):
         return HttpResponse(msg)
 
     # If the envelope is completed, pull out the PDFs from the notification XML
-    if envelope_data['envelope_status'] == "Completed":
+    if envelope_data["envelope_status"] == "Completed":
         try:
-            envelope_data['pdf_documents '] = docusign_pdf_files_saver(data, envelope_dir)
+            envelope_data["pdf_documents "] = docusign_pdf_files_saver(
+                data, envelope_dir
+            )
         except Exception as e:
             msg = str(e)
             logger.exception(msg)
@@ -145,9 +151,14 @@ def docusign_webhook_listener(request):
     logger.debug(envelope_data)
 
     # Updates Main Document Status
-    main_document = Document.objects.get(main_document=True, envelope_id=envelope_data['envelope_id'])
-    main_document.status = envelope_data['envelope_status']
-    esignature_log = DocumentESignatureLog(esignature_log=request.data['esignature_log'], document=main_document)
+    main_document = Document.objects.get(
+        main_document=True, envelope_id=envelope_data["envelope_id"]
+    )
+    main_document.status = envelope_data["envelope_status"]
+    esignature_log = DocumentESignatureLog(
+        esignature_log=envelope_data["envelope_all_details_message"],
+        document=main_document,
+    )
     esignature_log.save()
     main_document.save()
 
@@ -159,7 +170,7 @@ class DocumentViewSet(viewsets.ModelViewSet):
     serializer_class = DocumentSerializer
 
     def partial_update(self, request, *args, **kwargs):
-        ged_id = request.data['ged_id']
+        ged_id = request.data["ged_id"]
         instance = self.queryset.get(ged_id=ged_id)
         serializer = self.serializer_class(instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
@@ -189,6 +200,7 @@ class TenantViewSet(viewsets.ReadOnlyModelViewSet):
     """
     Returns a list or retrieves a tenant.
     """
+
     queryset = Tenant.objects.all()
     serializer_class = TenantSerializer
 
@@ -205,4 +217,3 @@ class TenantESignatureDataViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return TenantESignatureData.objects.filter(tenant=self.kwargs["pk"])
-
