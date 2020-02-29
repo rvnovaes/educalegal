e5d2712d65ee51af23628c0fbcf4a18571415ebf
rvnovaes <rvnovaes@gmail.com>
2020-02-29 13:39:02 -0300

    Changed service name from web to educa_legal and created mapping to media folder

--- api_requests/docassemble_api.py
+++ api_requests/docassemble_api.py
@@ -6,11 +6,11 @@ import requests
 # api_key = "GHHnjFWG9H4KKYYkMgd3yE0yPlNYMQ18"
 # server_url = "http://localhost/api/playground"
 ###### docs.silexsistemas.com.br #####
-api_key = "O2wG8BRr70yZnanUJmysCNLQ2nWG44cL"
-server_url = "https://docs.silexsistemas.com.br/api/playground"
+# api_key = "O2wG8BRr70yZnanUJmysCNLQ2nWG44cL"
+# server_url = "https://docs.silexsistemas.com.br/api/playground"
 ##### docs.educalegal.com.br #####
-# api_key = "C3vAIRNnr3BnJpKCdqlsXSV2fLWPKI0K"
-# server_url = "https://docs.educalegal.com.br/api/playground"
+api_key = "C3vAIRNnr3BnJpKCdqlsXSV2fLWPKI0K"
+server_url = "https://docs.educalegal.com.br/api/playground"
 
 project = "Development"
 
