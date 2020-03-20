517bca0abfff72d0c670c8ad52167b427d33ac9f
rvnovaes <rvnovaes@gmail.com>
2020-03-20 11:35:59 -0300

    Added debug toolbar

--- web/web/urls.py
+++ web/web/urls.py
@@ -37,3 +37,14 @@ urlpatterns = [
 if SILK:
     urlpatterns.append(url(r'^silk/', include('silk.urls', namespace='silk')))
 
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+
+                      # For django versions before 2.0:
+                      # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+                  ] + urlpatterns
+
