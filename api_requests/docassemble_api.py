c7316a742de393d424c814ee9eaf4fb44ac4b313
rvnovaes <rvnovaes@gmail.com>
2020-03-27 16:09:30 -0300

    Changed logic of docassemble_api.py to ask for user and server

--- api_requests/docassemble_api.py
+++ api_requests/docassemble_api.py
@@ -2,36 +2,10 @@ from os import listdir
 from os.path import isfile, join
 
 import requests
-###### LOCALHOST ######
-# iasmini
-# api_key = "CREI52r9wGtHRKTnTwoMw1lIwbfNA1tj"
-# luis paim
-# api_key = "Ti33Sv5KMNQCV9MDlwxY4RnBIbUWnAVD"
-# roberto
-# api_key = "CJrCAwt5Aul45RkOXeS2z9ZwISc6JkOf"
-# isabela
-# api_key = "YukwjUC4SWUZ0BZrIiLmY2UOQfYKFo4h"
-# server_url = "http://localhost/api/playground"
-###### teste.educalegal.com.br #####
-# usuario: teste@educalegal.com
-# api_key = "O2wG8BRr70yZnanUJmysCNLQ2nWG44cL"
-# server_url = "https://teste.educalegal.com.br/api/playground"
-###### Autotest teste.educalegal.com.br #####
-# usuario: teste@educalegal.com
-api_key = "O2wG8BRr70yZnanUJmysCNLQ2nWG44cL"
-server_url = "https://teste.educalegal.com.br/api/playground"
-project = "Autotest"
 
 
-##### docs.educalegal.com.br #####
-# api_key = "C3vAIRNnr3BnJpKCdqlsXSV2fLWPKI0K"
-# server_url = "https://docs.educalegal.com.br/api/playground"
-
-# project = "Development"
-
-
-def post_to_docassemble(api_key, server_url, path, destination, project="default"):
-    payload = {"key": api_key, "user_id": 1, "folder": destination, "project": project}
+def post_to_docassemble(api_key, server_url, path, destination, user_id, project="Development"):
+    payload = {"key": api_key, "user_id": user_id, "folder": destination, "project": project}
 
     files = dict()
     onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
@@ -48,14 +22,118 @@ def post_to_docassemble(api_key, server_url, path, destination, project="default
 
 
 if __name__ == "__main__":
+    destination = input(
+        """
+        Para onde você deseja enviar?:
+            1 - localhost - Development - Seu próprio playground na máquina local
+            2 - teste.educalegal.com.br - Development - Playground do seu usuário no servidor de teste
+            3 - teste.educalegal.com.br - Autotest - Playground do admin@admin.com
+            4 - docs.educalegal.com.br  - Autotest - Playground do admin@admin.com
+            5 - docs.educalegal.com.br  - Production - Playground do admin@admin.com
+    """
+    )
+
+    if destination == "1":
+        # 1 - localhost - Development - Seu próprio playground na máquina local
+        server_url = "http://localhost/api/playground"
+        project = "Development"
+        user_id = 1
+        print(
+            """
+        Você optou por enviar para seu playground na máquina local (localhost).
+        """
+        )
+        user = input(
+            """
+            Digite seu usuário:
+            1 - Iasmini
+            2 - Isabela
+            3 - Luis
+            4 - Roberto
+        """)
+        if user == "1":
+            # iasmini
+            api_key = "CREI52r9wGtHRKTnTwoMw1lIwbfNA1tj"
+        elif user == "2":
+            # isabela
+            api_key = "YukwjUC4SWUZ0BZrIiLmY2UOQfYKFo4h"
+        elif user == "3":
+            # luis paim
+            api_key = "Ti33Sv5KMNQCV9MDlwxY4RnBIbUWnAVD"
+        elif user == "4":
+            # roberto
+            api_key = "CJrCAwt5Aul45RkOXeS2z9ZwISc6JkOf"
+
+    if destination == "2":
+        # 2 - teste.educalegal.com.br - Development - Playground do seu usuário no servidor de teste
+        server_url = "https://teste.educalegal.com.br/api/playground"
+        project = "Development"
+        print(
+            """
+        Você optou por enviar para seu próprio playground no ambiente de teste.
+        """
+        )
+        user = input(
+            """
+            Digite seu usuário:
+            1 - Iasmini
+            2 - Isabela
+            3 - Luis
+            4 - Roberto
+        """)
+        if user == "1":
+            # iasmini
+            user_id = 4
+            api_key = ""
+        elif user == "2":
+            # isabela
+            user_id = 9
+            api_key = ""
+        elif user == "3":
+            # luis paim
+            user_id = 5
+            api_key = ""
+        elif user == "4":
+            # roberto
+            user_id = 14
+            api_key = "MNBexgT3cCDvmqXeKtUpp5YHMwfXWZxZ"
+    if destination == "3":
+        # teste.educalegal.com.br - Autotest - Playground do admin@admin.com
+        server_url = "https://teste.educalegal.com.br/api/playground"
+        project = "Autotest"
+        user_id = 1
+        api_key = "O2wG8BRr70yZnanUJmysCNLQ2nWG44cL"
+    if destination == "4":
+        # docs.educalegal.com.br - Autotest - Playground do admin@admin.com
+        server_url = "https://docs.educalegal.com.br/api/playground"
+        project = "Autotest"
+        user_id = 1
+        api_key = "C3vAIRNnr3BnJpKCdqlsXSV2fLWPKI0K"
+    if destination == "5":
+        # docs.educalegal.com.br - Production - Playground do admin@admin.com
+        server_url = "https://docs.educalegal.com.br/api/playground"
+        project = "Production"
+        user_id = 1
+        api_key = "C3vAIRNnr3BnJpKCdqlsXSV2fLWPKI0K"
+
+
+
     brcomeducalegal_questions = (
         "/opt/docassemble-brcomeducalegal/docassemble/brcomeducalegal/data/questions"
     )
     brcomeducalegal_templates = (
         "/opt/docassemble-brcomeducalegal/docassemble/brcomeducalegal/data/templates"
     )
-    brcomeducalegal_modules = "/opt/docassemble-brcomeducalegal/docassemble/brcomeducalegal/data/"
+    brcomeducalegal_modules = (
+        "/opt/docassemble-brcomeducalegal/docassemble/brcomeducalegal/data/"
+    )
 
-    post_to_docassemble(api_key, server_url, brcomeducalegal_questions, "questions", project)
-    post_to_docassemble(api_key, server_url, brcomeducalegal_templates, "templates", project)
-    post_to_docassemble(api_key, server_url, brcomeducalegal_modules, "modules", project)
+    post_to_docassemble(
+        api_key, server_url, brcomeducalegal_questions, "questions", user_id, project
+    )
+    post_to_docassemble(
+        api_key, server_url, brcomeducalegal_templates, "templates", user_id, project
+    )
+    post_to_docassemble(
+        api_key, server_url, brcomeducalegal_modules, "modules", user_id, project
+    )
