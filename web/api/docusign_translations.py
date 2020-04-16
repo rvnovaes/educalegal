dd6d08688558864d179493d0232311f5a54f91b4
Luispaim <luis.paimadv@gmail.com>
2020-04-16 15:48:35 -0300

    SLX-157-Traduzir os logs do docusign realizado

---
+++ web/api/docusign_translations.py
@@ -0,0 +1,30 @@
+envelope_statuses = {
+    "sent": "enviado",
+    "delivered": "entregue",
+    "completed": "completado",
+    "declined": "recusado",
+    "voided": "inválido",
+}
+
+recipient_statuses_dict = {
+    "Created": "criado",
+    "Sent": "enviado",
+    "Delivered": "entregue",
+    "Signed": "assinado",
+    "Declined": "recusado",
+    "Completed": "completado",
+    "Faxpending": "anexo de fax pendente",
+    "Autoresponded": "respondido automaticamente",
+}
+
+recipient_types_dict = {
+    "Agent": "agente",
+    "CarbonCopie": "em cópia",
+    "CertifiedDeliverie": "certificante",
+    "Editor": "editor",
+    "InPersonSigner": "destinatário pessoal",
+    "Intermediarie": "intermediário",
+    "Seal": "selo",
+    "Signer": "assinante",
+    "Witness": "testemunha",
+}
