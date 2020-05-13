4dfe01491948c08cc24cdd077ad5085f9e731923
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-05-13 14:53:26 -0300

    slx-164: usando urljoin para montar a url da entrevista

--- web/interview/columns.py
+++ web/interview/columns.py
@@ -1,4 +1,5 @@
 from django_tables2 import TemplateColumn
+from urllib.parse import urljoin
 
 
 class InterviewCustomUrlColumn(TemplateColumn):
@@ -10,16 +11,14 @@ class InterviewCustomUrlColumn(TemplateColumn):
             final_url = record.base_url
         else:
             # monta url da entrevista de acordo com parametros da configuracao do servidor escolhido
-            interview_server_config = record.interview_server_config
-            if interview_server_config.base_url[-1] == "/":
-                url = interview_server_config.base_url
-            else:
-                url = interview_server_config.base_url + "/"
-            user_id = interview_server_config.user_id
-            project_name = interview_server_config.project_name
-            yaml_name = record.yaml_name
-            final_url = "{url}interview?i=docassemble.playground{user_id}{project_name}%3A{yaml_name}".format(
-                url=url, user_id=user_id, project_name=project_name, yaml_name=yaml_name
+            isc = record.interview_server_config
+            final_url = urljoin(
+                isc.base_url,
+                "interview?i=docassemble.playground{user_id}{project_name}%3A{yaml_name}".format(
+                    user_id=isc.user_id,
+                    project_name=isc.project_name,
+                    yaml_name=record.yaml_name,
+                ),
             )
         # adds access parameters to URL
         if record.is_generic is True:
