f6931bf7e6a9d61822a1af8e04174877527ab0d2
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-05-14 11:17:43 -0300

    Acertos na tarefa de sincronizacao adicionados drop e create

--- web/pg_dump_digital_ocean_to_local_educalegal_app.sh
+++ web/pg_dump_digital_ocean_to_local_educalegal_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PGPASSFILE="/opt/educalegal/web/pgpass"
+export PGPASSFILE="/opt/educalegal/web/pgpass_do"
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 FILENAME=educa-legal-app-${DATE_WITH_TIME}.sql
 pg_dump --host=educa-legal-producao-db-postgresql-do-user-106912-0.db.ondigitalocean.com \
@@ -9,5 +9,7 @@ pg_dump --host=educa-legal-producao-db-postgresql-do-user-106912-0.db.ondigitalo
   --no-password \
   --verbose \
   --clean > ${FILENAME}
+PGPASSWORD=educalegal dropdb -U educalegal -h localhost -p 7654 --echo educalegal
+PGPASSWORD=educalegal createdb -U educalegal -h localhost -p 7654 --echo educalegal
 PGPASSWORD=educalegal psql -U educalegal -d educalegal -h localhost -e -p 7654 < ${FILENAME}
 rm ${FILENAME}
