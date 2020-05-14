b962670aad493b5d8cba185c665339a1d47138b1
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-05-14 10:47:44 -0300

    Criada tarefa do make para baixar o banco remoto do app no container do banco na maquina local

---
+++ web/pg_dump_digital_ocean_to_local_educalegal_app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export PGPASSFILE="/opt/educalegal/web/pgpass"
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
+FILENAME=educa-legal-app-${DATE_WITH_TIME}.sql
+pg_dump --host=educa-legal-producao-db-postgresql-do-user-106912-0.db.ondigitalocean.com \
+  --port=25060 \
+  --dbname=educa-legal-app \
+  --username=educa-legal-app \
+  --no-password \
+  --verbose \
+  --clean > ${FILENAME}
+PGPASSWORD=educalegal psql -U educalegal -d educalegal -h localhost -e -p 7654 < ${FILENAME}
+rm ${FILENAME}
