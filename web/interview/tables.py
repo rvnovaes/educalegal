35e75549a926d8f097935fb5f44450bf77249256
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-05-19 15:11:31 -0300

    slx-164: ocultada coluna de geracao em lote se o plano do tenant nao usar bulk_interview

--- web/interview/tables.py
+++ web/interview/tables.py
@@ -6,13 +6,6 @@ from .columns import InterviewCustomUrlColumn, BulkInterviewColumn
 
 class InterviewTable(tables.Table):
     date_available = tables.DateTimeColumn(format="d/m/Y")
-
-    class Meta:
-        model = Interview
-        order_by = "name"
-        template_name = "django_tables2/bootstrap4.html"
-        fields = ("name", "description", "version", "date_available", "base_url", "bulk_interview")
-
     base_url = InterviewCustomUrlColumn(
         template_name="interview/criar_button.html", verbose_name="Criar", orderable=False
     )
@@ -20,3 +13,15 @@ class InterviewTable(tables.Table):
     bulk_interview = BulkInterviewColumn(
         template_name="interview/bulk_button.html", verbose_name="Em lote", orderable=False
     )
+
+    def before_render(self, request):
+        if not request.user.tenant.plan.use_bulk_interview:
+            self.columns.hide('bulk_interview')
+
+    class Meta:
+        model = Interview
+        order_by = "name"
+        template_name = "django_tables2/bootstrap4.html"
+        fields = ("name", "description", "version", "date_available", "base_url")
+
+
