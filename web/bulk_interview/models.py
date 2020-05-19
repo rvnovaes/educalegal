36ddde11167c76cca8183c936bfbe8d57efe28e4
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-05-18 22:11:12 -0300

    slx-164: Adicionado modelo de entrevista em lote

--- web/bulk_interview/models.py
+++ web/bulk_interview/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+
 
 from tenant.models import TenantAwareModel
 from interview.models import Interview
@@ -9,5 +11,8 @@ class BulkGeneration(TenantAwareModel):
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Criação")
     interview = models.ForeignKey(
         Interview, null=True, on_delete=models.CASCADE, verbose_name="Modelo"
+    )
+    interview_fields = JSONField(
+        null=True, verbose_name="Campos e Tipos de Campo do Documento"
     )
     source_file = models.FileField()
