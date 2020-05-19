e4e38b728e70551c020664f2bdde76b54127eae3
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-05-18 22:10:21 -0300

    slx-164: Correcoes no cliente do Docassemble. Adicao de metodo para executar acao

--- web/bulk_interview/docassemble_client.py
+++ web/bulk_interview/docassemble_client.py
@@ -13,18 +13,15 @@ class DocassembleClient:
         self.api_base_url = base_url
         headers = {"X-API-Key": admin_key}
         self.session = retry(
-            Session(),
-            retries=3,
-            backoff_factor=1,
-            status_to_retry=(500, 502, 504),
+            Session(), retries=3, backoff_factor=1, status_to_retry=(500, 502, 504),
         )
         self.session.headers.update(headers)
 
-    def secret_read(self, username, password):
+    def secret_read(self, key, username, password):
         """ Obtain a decryption key for a user """
 
         final_url = self.api_base_url + "/api/secret"
-        payload = {'username': username, 'password': password}
+        payload = {"key": key, "username": username, "password": password}
         response = self.session.get(final_url, params=payload)
 
         return response.json()
@@ -44,33 +41,58 @@ class DocassembleClient:
         response = self.session.get(final_url)
         return response.json()
 
-    def session_read(self, interview_name):
+    def start_interview(self, interview_name, secret):
         """ Start an interview session """
 
         final_url = self.api_base_url + "/api/session/new"
-        payload = {'i': interview_name}
+        payload = {"i": interview_name, "secret": secret}
 
         try:
             response = self.session.get(final_url, params=payload).json()
-            session = response['session']
+            session = response["session"]
             return session
         except Exception:
             extype, ex, tb = sys.exc_info()
-            message = "Erro ao criar nova sessão. Response: {response}. Exception: {ex}".format(response=response,
-                                                                                                ex=ex.__str__())
+            message = "Erro ao criar nova sessão. Response: {response}. Exception: {ex}".format(
+                response=response, ex=ex.__str__()
+            )
             raise Exception(message).with_traceback(ex.__traceback__)
 
-    def interview_set_variables(self, secret, interview_name, variables):
+    def interview_set_variables(self, secret, interview_name, variables, session):
         """ Set variables in an interview
             variables - dict with all the variables for the interview
         """
-        session = self.session_read(interview_name)
 
         final_url = self.api_base_url + "/api/session"
-        data = {'i': interview_name, 'session': session, 'secret': secret, 'variables': json.dumps(variables)}
+
+        payload = {
+            "i": interview_name,
+            "session": session,
+            "secret": secret,
+            "variables": json.dumps(variables),
+        }
 
         try:
-            response = self.session.post(final_url, data=data)
+            response = self.session.post(final_url, data=payload)
             return response.json(), response.status_code
         except Exception as e:
-            print('Erro: ', e.__class__.__name__)
+            print("Erro: ", e.__class__.__name__)
+
+    def interview_run_action(
+        self, secret, interview_name, session, action, action_arguments=None
+    ):
+        final_url = self.api_base_url + "/api/session/action"
+
+        payload = {
+            "i": interview_name,
+            "session": session,
+            "secret": secret,
+            "action": action,
+            action_arguments: action_arguments,
+        }
+
+        try:
+            response = self.session.post(final_url, data=payload)
+            return response.status_code
+        except Exception as e:
+            print("Erro: ", e.__class__.__name__)
