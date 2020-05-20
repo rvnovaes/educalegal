a69378e662ec28be70e4c396c666e0a649a2db86
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-05-19 21:47:08 -0300

    slx-164: Primeira veresao finalizada de mongo_util.py

--- web/mongo_util/mongo_util.py
+++ web/mongo_util/mongo_util.py
@@ -6,57 +6,40 @@ def create_mongo_connection(db, alias, username, password, host, port):
     connect(db, alias, username=username, password=password, host=host, port=port)
 
 
-def get_field(field_name):
-    if field_name == "BooleanField":
-        return BooleanField()
-    if field_name == "DateTimeField":
-        return DateTimeField()
-    if field_name == "DecimalField":
-        return DecimalField()
-    if field_name == "EmailField":
-        return EmailField()
-    if field_name == "FloatField":
-        return EmailField()
-    if field_name == "IntField":
-        return IntField()
-    if field_name == "LongField":
-        return IntField()
-    if field_name == "StringField":
-        return StringField()
+def get_field(field_type, required):
+    if field_type == "BooleanField":
+        field = BooleanField()
+    if field_type == "DateTimeField":
+        field = DateTimeField()
+    if field_type == "DecimalField":
+        field = DecimalField()
+    if field_type == "EmailField":
+        field = EmailField()
+    if field_type == "FloatField":
+        field = EmailField()
+    if field_type == "IntField":
+        field = IntField()
+    if field_type == "LongField":
+        field = IntField()
+    if field_type == "StringField":
+        field = StringField()
+    if required:
+        field.required = True
+    return field
 
 
-def create_dynamic_document_class(class_name: str, fields_dict: dict):
+def create_dynamic_document_class(class_name: str, field_properties_dict: dict, required_properties_dict: dict):
     # Cria o dicionario de atributos que será usado na classe dinamica
     custom_attributes = dict()
-    for key, value in fields_dict.items():
+    for key, value in field_properties_dict.items():
+        required = required_properties_dict[key]
+        if required == "true":
+            required = True
+        else:
+            required = False
         key = key.replace('.', '_')
-        custom_attributes[key] = get_field(value)
+        custom_attributes[key] = get_field(value, required)
     # Cria a classe de documento dinamica
-    DinamicDocumentClass = type(class_name, (Document,), custom_attributes)
+    DynamicDocumentClass = type(class_name, (Document,), custom_attributes)
 
-    return DinamicDocumentClass
-
-
-
-
-
-if __name__ == "__main__":
-    create_mongo_connection(
-        "educalegal", "default", "educalegal", "educalegal", "localhost", 27017
-    )
-    class_name = "contrato_de_prestacao_de_servicos_educacionais"
-    atrributes = dict()
-    with open("contrato-prestacao-servicos-educacionais_v1.csv") as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=",")
-        rows = [r for r in reader]
-    # Cria o dicionario de atributos que será usado na classe dinamica
-    DynamicDocumentClass = create_dynamic_document_class(rows[0])
-
-    # Grava cada uma das linhas
-    normalized_headers_row = dict()
-    for row in rows[1:]:
-        for key, value in row.items():
-            key = key.replace('.', '_')
-            normalized_headers_row[key] = value
-        dynamic_document = DynamicDocumentClass(**normalized_headers_row)
-        dynamic_document.save()
+    return DynamicDocumentClass
