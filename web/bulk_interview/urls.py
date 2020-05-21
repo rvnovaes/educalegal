485d8fbc8afaaad122750b18f07e562a49c0e7d5
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-05-21 15:42:49 -0300

    slx-164: Remocao da importacao direta do CSV

--- web/bulk_interview/urls.py
+++ web/bulk_interview/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import (
     ValidadeCSVFile,
     generate_bulk_documents,
-    bulk_interview
 )
 
 app_name = "bulk_interview"
@@ -10,6 +9,5 @@ app_name = "bulk_interview"
 urlpatterns = [
     path("bulk_interview/validate/<int:interview_id>", ValidadeCSVFile.as_view(), name="bulk_interview_validate_generate"),
     path("bulk_interview/generate/<int:bulk_generation_id>", generate_bulk_documents, name="bulk_interview_generation_result"),
-    path("bulk_interview/<int:interview_id>", bulk_interview, name="bulk_interview"),
 ]
 
