70a99573b7054fb7f467b5ff93e7be16abf8a3f4
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-05-28 06:44:28 -0300

    slx-164: Adicionados campos de school names e school units a bulk generation

--- web/bulk_interview/models.py
+++ web/bulk_interview/models.py
@@ -12,8 +12,10 @@ class BulkGeneration(TenantAwareModel):
     interview = models.ForeignKey(
         Interview, null=True, on_delete=models.CASCADE, verbose_name="Modelo"
     )
-    field_types_dict = JSONField(null=False, default={}, verbose_name="Campos do Modelo")
-    required_fields_dict = JSONField(null=False, default={}, verbose_name="Obrigatoriedade dos Campos do Modelo")
+    field_types_dict = JSONField(null=False, default=dict, verbose_name="Campos do Modelo")
+    required_fields_dict = JSONField(null=False, default=dict, verbose_name="Obrigatoriedade dos Campos do Modelo")
+    school_names_set = JSONField(null=False, default=dict, verbose_name="Conjunto de Nomes de Escola")
+    school_units_names_set = JSONField(null=False, default=dict, verbose_name="Conjunto de Nomes de Unidades Escolares")
     mongo_db_collection_name = models.CharField(
         max_length=1024, null=False, verbose_name="Coleção de Documentos no Mongo"
     )
