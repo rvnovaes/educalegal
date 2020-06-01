4dd5b28d5a902d926bc06e2391924ee9298672b7
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-06-01 11:16:50 -0300

    Testes de geracao de documentos via API

--- api_requests/docassemble_api_requests.py
+++ api_requests/docassemble_api_requests.py
@@ -1,21 +1,22 @@
+import json
 from web.bulk_interview.docassemble_client import DocassembleClient
 
 ###### LOCALHOST ######
 # api_base_url = "http://localhost"
-# key = "u0AFroAWHD1XF5hQSv0qdzKEaifI7imK"
+# key = "75FjwSEvktr5AhC2BRhHxUArOjGpKHTb"
+# interview_name = 'docassemble.playground1Development:entrevista-debug-bulk.yml'
 # interview_name = 'docassemble.playground1Development:favorite.yml'
 # interview_name = 'docassemble.playground1Development:contrato-prestacao-servicos-educacionais.yml'
+
 ###### docs.educalegal ######
 # api_base_url = "https://docs.educalegal.com.br"
-# key = 'OkHYL2fYJApLfjwTeM2gRUZfybEzbqy5'
-# interview_name = 'docassemble.playground4Development:contrato-prestacao-servicos-educacionais.yml'
+# key = 'C3vAIRNnr3BnJpKCdqlsXSV2fLWPKI0K'
+# interview_name = 'docassemble.playground1Production:entrevista-debug-bulk.yml'
 
 ###### generation.educalegal ######
 api_base_url = "https://generation.educalegal.com.br"
 key = "D8WUkJKxxW06qDS1UB5KlwyVXeUE1Mnx"
-interview_name = (
-    "docassemble.playground1Production:contrato-prestacao-servicos-educacionais.yml"
-)
+interview_name = 'docassemble.playground1Production:entrevista-debug-bulk.yml'
 
 username = "admin@admin.com"
 user_password = "Silex2109"
@@ -29,12 +30,9 @@ user_password = "Silex2109"
 
 if __name__ == "__main__":
 
-    payload = {
-        "i": "docassemble.playground1Production:contrato-prestacao-servicos-educacionais.yml",
-        "session": "louAluXimJR47NwEpFaZFSQgKVvFiA4X",
-        "secret": "127db081a79dead3",
-        "variables": '{"id": "5ecef2617d90fa77d22ed357", "obs": "Desconto 10%", "rgAluno": "17.817.809-3", "cepAluno": "21381-040", "cpfAluno": "677.423.294-92", "desconto": "Sim", "parcelas": "\\u00e0 vista", "ruaAluno": "Travessa Jo\\u00e3o de Matos", "anoLetivo": 2020, "nomeAluno": "Gabriel Renato Kau\\u00ea Ara\\u00fajo", "numbAluno": "192", "serieAluno": "1\\u00aa s\\u00e9rie do ensino fundamental", "valorAnual": 5000.0, "bairroAluno": "Quintino Bocai\\u00fava", "cidadeAluno": "Rio de Janeiro", "compleAluno": "Apto 400", "estadoAluno": "RJ", "periodoAluno": "matutino", "unidadeAluno": "Super Bacana", "signature_date": "09/04/2020", "primeiraParcela": "\\u00e0 vista", "selected_school": "Col\\u00e9gio Bacana", "signature_local": "Belo Horizonte", "nacionalidadeAluno": "brasileiro(a)", "vencimentoParcelas": "dia 05 de cada m\\u00eas a partir de janeiro de 2020", "created": "2020-05-27T20:06:09.211000", "contratantes": {"elements": [{"nacionalidade": "brasileiro(a)", "estadocivil": "casado(a)", "prof": "engenheira", "cpf": "171.668.857-42", "rg": "12.531.478-4", "telefone": "(21) 2885-1328", "wtt": "(21) 98461-9677", "email": "rvnovaes@gmail.com", "cep": "21381-040", "rua": "Travessa Jo\\u00e3o de Matos", "numb": "192", "complemento": "2", "bairro": "Quintino Bocai\\u00fava", "cidade": "Rio de Janeiro", "estado": "RJ", "name": {"first": "Laura S\\u00f4nia J\\u00e9ssica Santos", "_class": "docassemble.base.util.IndividualName", "uses_parts": true, "instanceName": "contratantes[0].name"}, "instanceName": "contratantes[0]", "_class": "docassemble.base.util.Individual"}], "auto_gather": "False", "gathered": "True", "_class": "docassemble.base.core.DAList", "instanceName": "contratantes"}, "content_document": "contrato-prestacao-servicos-educacionais.docx", "valid_contratantes_table": "continue", "submit_to_esignature": "True", "url_args": {"tid": 2, "ut": "48974bcc7b577ab3d6fed7c281d90324f4612810", "intid": 2}}',
-    }
+    payload = {"id": "5ed031f7bb0cb6720079cce8", "nomeAluno": "Plat\u00e3o de Atenas", "unidadeAluno": "Super Bacana", "selected_school": "Col\u00e9gio Bacana", "created": "2020-05-28T18:49:43.705000", "submit_to_esignature": "False", "url_args": {"tid": 2, "ut": "48974bcc7b577ab3d6fed7c281d90324f4612810", "intid": 2}}
+
+
 
     dac = DocassembleClient(api_base_url, key)
 
@@ -58,6 +56,7 @@ if __name__ == "__main__":
 
     print(status_code)
     print(response)
+    print(response.json())
 
     # print(dac.user_interviews_list())
 
