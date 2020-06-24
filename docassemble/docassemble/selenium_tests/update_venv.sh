dbbe4e3a7984b488c1c93c2aaa7a7f7fac3b215b
iasmini <iasmini.gomes@gmail.com>
2020-06-24 11:05:02 -0300

    slx-247: alterado diretorio da venv

--- docassemble/docassemble/selenium_tests/update_venv.sh
+++ docassemble/docassemble/selenium_tests/update_venv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /opt/venvs/docassemble-brcomeducalegal/bin/activate
+source /opt/venvs/educalegal/bin/activate
 pip install --upgrade -r /var/lib/jenkins/workspace/AutotestPipeline/docassemble/selenium_tests/requirements.txt
