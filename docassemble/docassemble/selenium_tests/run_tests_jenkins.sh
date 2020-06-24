23ca39db656ff87c53511263f300569bd6bd2f33
iasmini <iasmini.gomes@gmail.com>
2020-06-24 18:09:24 -0300

    slx-247: alterado caminho do requirements no sh

--- docassemble/docassemble/selenium_tests/run_tests_jenkins.sh
+++ docassemble/docassemble/selenium_tests/run_tests_jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /opt/venvs/educalegal/bin/activate
-pip3 install -r /var/lib/jenkins/workspace/Autotest Pipeline/docassemble/selenium_tests/requirements.txt
-cd /var/lib/jenkins/workspace/Autotest Pipeline/docassemble/selenium_tests
+pip3 install -r /var/lib/jenkins/workspace/Autotest Pipeline/docassemble/docassemble/selenium_tests/requirements.txt
+cd /var/lib/jenkins/workspace/Autotest Pipeline/docassemble/docassemble/selenium_tests
 pytest
