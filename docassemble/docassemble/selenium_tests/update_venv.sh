03923900954d4e27f5250f430141ef8c351ed624
iasmini <iasmini.gomes@gmail.com>
2020-06-24 18:10:53 -0300

    slx-247: incluida aspas no nome do caminho

--- docassemble/docassemble/selenium_tests/update_venv.sh
+++ docassemble/docassemble/selenium_tests/update_venv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/venvs/educalegal/bin/activate
-pip3 install --upgrade -r /var/lib/jenkins/workspace/Autotest Pipeline/docassemble/docassemble/selenium_tests/requirements.txt
+pip3 install --upgrade -r /var/lib/jenkins/workspace/'Autotest Pipeline'/docassemble/docassemble/selenium_tests/requirements.txt
