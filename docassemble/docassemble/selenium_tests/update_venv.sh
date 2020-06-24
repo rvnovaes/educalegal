d745190a35ebb2912d4a958349af74566b4ea4a2
iasmini <iasmini.gomes@gmail.com>
2020-06-24 11:11:38 -0300

    slx-247: alterado pra pip3. nao estava achando pip

--- docassemble/docassemble/selenium_tests/update_venv.sh
+++ docassemble/docassemble/selenium_tests/update_venv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/venvs/educalegal/bin/activate
-pip install --upgrade -r /var/lib/jenkins/workspace/AutotestPipeline/docassemble/selenium_tests/requirements.txt
+pip3 install --upgrade -r /var/lib/jenkins/workspace/AutotestPipeline/docassemble/selenium_tests/requirements.txt
