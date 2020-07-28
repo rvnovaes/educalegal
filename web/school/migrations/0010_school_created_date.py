df5a4b667428f719465fd204ebd313f795e68d73
Luispaim <luis.paimadv@gmail.com>
2020-07-28 16:44:34 -0300

    slx-287: Data de cadatro na escola incluída

--- web/school/migrations/0010_school_created_date.py
+++ web/school/migrations/0010_school_created_date.py
@@ -2,23 +2,26 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("school", "0009_auto_20200721_1103"),
     ]
-
     operations = [
         migrations.AddField(
             model_name='school',
             name='created_date',
-            field=models.DateTimeField(auto_now_add=True, verbose_name="Criação"),
+            field=models.DateTimeField(auto_now_add=True, null=True, blank=True, verbose_name="Criação"),
         ),
-        migrations.RunSQL("""update 
-  school_school e  
-set 
-  created_date = d.created_date
-from 
-  tenant_tenant d
-where 
-  e.tenant_id = d.id;""")
+        migrations.RunSQL("""update
+  school_school s
+set
+  created_date = t.created_date
+from
+  tenant_tenant t
+where
+  s.tenant_id = t.id;"""),
+        migrations.AlterField(
+            model_name='school',
+            name='created_date',
+            field=models.DateTimeField(auto_now_add=True, blank=True, verbose_name="Criação"),
+        ),
     ]
