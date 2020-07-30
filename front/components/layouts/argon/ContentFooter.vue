1838b663ea1e2d70f00a5dbf7a7c03852bd3fac8
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-07-30 18:33:29 -0300

    slx-279: Modificado componente de rodape

--- front/components/layouts/argon/ContentFooter.vue
+++ front/components/layouts/argon/ContentFooter.vue
@@ -2,26 +2,9 @@
   <footer class="footer px-4 mx-2">
     <div class="row align-items-center justify-content-lg-between">
       <div class="col-lg-6">
-        <div class="copyright text-center text-lg-left text-muted">
-          © {{year}} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank" rel="noopener">Creative Tim</a> &
-          <a href="https://www.binarcode.com?ref=creative-tim" class="font-weight-bold ml-1" target="_blank" rel="noopener">Binar Code</a>
+        <div class="copyright text-center text-black-50 text-lg-right">
+          © {{year}} <a href="https://www.educalegal.com.br" class="font-weight-bold ml-1" target="_blank" rel="noopener">Educa Legal</a> - Versão 2.0
         </div>
-      </div>
-      <div class="col-lg-6">
-        <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-          <li class="nav-item">
-            <a href="https://www.creative-tim.com" class="nav-link" target="_blank" rel="noopener">Creative Tim</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank" rel="noopener">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a href="http://blog.creative-tim.com" class="nav-link" target="_blank" rel="noopener">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.creative-tim.com/license" class="nav-link" target="_blank" rel="noopener">License</a>
-          </li>
-        </ul>
       </div>
     </div>
   </footer>
