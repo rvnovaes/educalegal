c8d4887945c42938d0110f7f931da608ddf9dbbe
iasmini <iasmini.gomes@gmail.com>
2020-08-04 19:00:26 -0300

    slx-284: testado upload_document

--- docassemble/docassemble/brcomeducalegal/data/module_clicksign_client.py
+++ docassemble/docassemble/brcomeducalegal/data/module_clicksign_client.py
@@ -1,15 +1,9 @@
-import base64
-
 from requests import Session
 
 # https://github.com/bustawin/retry-requests
 from retry_requests import retry
 
-__all__ = ["ClickSignClient", "make_document_base64"]
-
-###### LOCALHOST ######
-api_base_url = "https://sandbox.clicksign.com/"
-token = "dc0251e3-bb8e-4813-84c0-1158ba0bdbcf"
+__all__ = ["ClickSignClient"]
 
 
 class ClickSignClient:
@@ -30,46 +24,19 @@ class ClickSignClient:
         response = self.session.get(final_url).json()
         return response
 
-    def upload_document(self, document_name, document_path):
-        """
-        Envia o documento para o ClickSign.
-        :param document_name:
-         Nome do arquivo que será criado no ClickSign
-        :param document_path:
-        Caminho onde o documento será criado dentro da Clicksign, incluindo o nome do arquivo.
-        - Campo deverá começar com "/".
-        - Se a pastas e as subpastas não existirem, elas serão criadas automaticamente.
-        - Esse campo não representa o caminho do seu servidor.
-        :return:
-        JSON com os dados do documento criado.
-        """
-        content_base64 = 'data:application/pdf;base64,' + make_document_base64(document_path)
-
-        payload = {
-            "document": {
-                "path": "/" + document_name,
-                "content_base64": content_base64,
-                "sequence_enabled": True
-                }
-            }
-
-        final_url = self.api_base_url + "api/v1/documents"
-        response = self.session.post(final_url, json=payload).json()
-        return response
-
-    def upload_document2(self, documents):
+    def upload_document(self, documents):
         for document in documents:
             payload = {
                 "document": {
                     "path": "/" + document['name'],
-                    "content_base64": document['documentBase64'],
+                    "content_base64": 'data:application/pdf;base64,' + document['documentBase64'],
                     "sequence_enabled": True
                     }
                 }
 
         final_url = self.api_base_url + "api/v1/documents"
-        response = self.session.post(final_url, json=payload).json()
-        return response
+        response = self.session.post(final_url, json=payload)
+        return response.status_code, response.json()
 
     def add_signer(self, recipients):
         """
@@ -125,23 +92,12 @@ class ClickSignClient:
         return response
 
     def send_to_cliksign(self, documents, recipients):
-        document_response = self.upload_document2(documents)
-        signer_response = self.add_signer(recipients)
-        signer_to_document_response = self.add_signer_to_document(
+        status_code, document_response = self.upload_document2(documents)
+
+        if status_code == 200:
+            status_code, signer_response = self.add_signer(recipients)
+
+        status_code, signer_to_document_response = self.add_signer_to_document(
             document_response['document']['key'], signer_response)
 
         return signer_to_document_response
-
-
-def make_document_base64(document_path):
-    """Converts your document from document_path to a base64 string, as used by Docusign"""
-    with open(document_path, "rb") as document:
-        return base64.b64encode(document.read()).decode("utf-8")
-
-
-if __name__ == '__main__':
-    csc = ClickSignClient(api_base_url, token)
-
-    # print(csc.get_document('9afc2318-4729-4764-810b-ac66c6e6571e'))
-
-    print(csc.send_document('lorem-ipsum.pdf', '/opt/educalegal/api_requests/lorem-ipsum.pdf'))
