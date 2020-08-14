30c70f82ef507ec835450e2932eb4edb12f377d1
iasmini <iasmini.gomes@gmail.com>
2020-08-14 15:06:08 -0300

    slx-295: incluido esignature_app na lista e na edicao

--- web/tenant/admin.py
+++ web/tenant/admin.py
@@ -4,9 +4,9 @@ from .models import Tenant, TenantGedData, ESignatureApp
 
 @admin.register(Tenant)
 class TenantAdmin(admin.ModelAdmin):
-    list_display = ["name", "plan", "auto_enrolled", "created_date", "phone"]
+    list_display = ["name", "plan", "esignature_app", "auto_enrolled", "phone", "created_date"]
     list_filter = ["auto_enrolled", "plan"]
-    list_editable = ["plan"]
+    list_editable = ["plan", "esignature_app"]
     search_fields = ["name"]
 
 
