7e443931df1978bc76d5677a4a807e9783084f5c
Roberto Novaes <rnovaes@ufmg.br>
2020-08-20 09:50:47 -0300

    slx-279: reformatado o codigo de schools.js

--- front/store/schools.js
+++ front/store/schools.js
@@ -1,71 +1,71 @@
 export const state = () => ({
   schools: [],
-})
+});
 
 export const mutations = {
-  addSchool (state, school) {
-    state.schools.push(school)
+  addSchool(state, school) {
+    state.schools.push(school);
   },
-  setSchools (state, schools) {
-    state.schools = schools
+  setSchools(state, schools) {
+    state.schools = schools;
   },
-  deleteSchool(state, school){
-    const i = state.schools.map(school => school.id).indexOf(school.id)
-    state.schools.splice(i, 1)
+  deleteSchool(state, school) {
+    const i = state.schools.map(school => school.id).indexOf(school.id);
+    state.schools.splice(i, 1);
   },
-  updateName (state, payload) {
-    state.schools.find(school => school.id === Number(payload.id)).name = payload.name
+  updateName(state, payload) {
+    state.schools.find(school => school.id === Number(payload.id)).name = payload.name;
   },
-  updateLegalName (state, payload) {
-    state.schools.find(school => school.id === Number(payload.id)).legal_name = payload.legal_name
+  updateLegalName(state, payload) {
+    state.schools.find(school => school.id === Number(payload.id)).legal_name = payload.legal_name;
   },
-  updateCNPJ (state, payload) {
-    state.schools.find(school => school.id === Number(payload.id)).cnpj = payload.cnpj
+  updateCNPJ(state, payload) {
+    state.schools.find(school => school.id === Number(payload.id)).cnpj = payload.cnpj;
   },
-  updateLegalNature (state, payload) {
-    state.schools.find(school => school.id === Number(payload.id)).legal_nature = payload.legal_nature
+  updateLegalNature(state, payload) {
+    state.schools.find(school => school.id === Number(payload.id)).legal_nature = payload.legal_nature;
   },
-  updateCity (state, payload) {
-    state.schools.find(school => school.id === Number(payload.id)).city = payload.city
+  updateCity(state, payload) {
+    state.schools.find(school => school.id === Number(payload.id)).city = payload.city;
   },
-  updateUF (state, payload) {
-    state.schools.find(school => school.id === Number(payload.id)).state = payload.state
+  updateUF(state, payload) {
+    state.schools.find(school => school.id === Number(payload.id)).state = payload.state;
   }
-}
+};
 
 export const getters = {
-  getAllSchools(state){
-    return state.schools
+  getAllSchools(state) {
+    return state.schools;
   },
   // https://vuex.vuejs.org/guide/getters.html#method-style-access
-  getSchool:(state) => (id) => {
-    return state.schools.find(school => school.id === Number(id))
+  getSchool: (state) => (id) => {
+    return state.schools.find(school => school.id === Number(id));
   }
-}
+};
 
 export const actions = {
   fetchAllSchools({commit}) {
-    return this.$axios.get('/v2/tenant/schools').then(res => {
+    return this.$axios.get("/v2/tenant/schools").then(res => {
       if (res.status === 200) {
-        commit('setSchools', res.data.results)
+        commit("setSchools", res.data.results);
       }
-    })
+    });
   },
   deleteSchool({commit}, school) {
     return this.$axios.delete(`/v2/tenant/schools/${school.id}`).then(res => {
       if (res.status === 200) {
-        console.log(res)
-        commit('deleteSchool', school)
+        console.log(res);
+        commit("deleteSchool", school);
       }
-    })
+    });
   },
   createSchool({commit}, school) {
     return this.$axios.post(`/v2/tenant/schools/`, school).then(res => {
       if (res.status === 200) {
-        console.log(res)
-        commit('addSchool', school)
+        console.log(res);
+        commit("addSchool", school);
       }
-    })
+    });
   }
 
   // fetchSchool({commit}, id) {
@@ -75,4 +75,4 @@ export const actions = {
   //     }
   //   })
   // },
-}
+};
