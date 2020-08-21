ba28a96682bb1e75208afe489f0834917c1041f1
Roberto Novaes <rnovaes@ufmg.br>
2020-08-21 15:56:48 -0300

    slx-279: modificado nuxt config para pegar variavel de ambiente para a API

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,52 +1,58 @@
-const pkg = require('./package')
-console.log('ENV', process.env.NODE_ENV)
+const pkg = require("./package");
+const NODE_ENV = process.env.NODE_ENV;
+console.log(NODE_ENV)
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
   router: {
-    base: '/',
-    linkExactActiveClass: 'active',
+    base: "/",
+    linkExactActiveClass: "active",
     // Força autenticacao para acessar as paginas
-    middleware: ['auth'],
+    middleware: ["auth"],
   },
   /*
   ** Headers of the page
   */
   head: {
-    title: 'Educa Legal',
+    title: "Educa Legal",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Educa Legal - Advocacia Virtual para Escolas' }
+      {charset: "utf-8"},
+      {name: "viewport", content: "width=device-width, initial-scale=1"},
+      {hid: "description", name: "description", content: "Educa Legal - Advocacia Virtual para Escolas"}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700'},
-      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.3/css/all.css', integrity: "sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/", crossorigin: "anonymous"}
+      {rel: "icon", type: "image/x-icon", href: "favicon.ico"},
+      {rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"},
+      {
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.6.3/css/all.css",
+        integrity: "sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/",
+        crossorigin: "anonymous"
+      }
     ]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: "#fff"},
 
   /*
   ** Global CSS
   */
   css: [
-    'assets/css/nucleo/css/nucleo.css',
-    'assets/sass/argon.scss'
+    "assets/css/nucleo/css/nucleo.css",
+    "assets/sass/argon.scss"
   ],
 
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/dashboard/dashboard-plugin',
-    '~/plugins/axios-educa-legal',
-    {src: '~/plugins/dashboard/full-calendar', ssr: false },
-    {src: '~/plugins/dashboard/world-map', ssr: false },
+    "~/plugins/dashboard/dashboard-plugin",
+    "~/plugins/axios-educa-legal",
+    {src: "~/plugins/dashboard/full-calendar", ssr: false},
+    {src: "~/plugins/dashboard/world-map", ssr: false},
   ],
 
   /*
@@ -54,10 +60,10 @@ module.exports = {
   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/auth',
-    '@nuxtjs/toast',
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa",
+    "@nuxtjs/auth",
+    "@nuxtjs/toast",
   ],
 
   /*
@@ -69,32 +75,37 @@ module.exports = {
     // Defines the base URL which is used and prepended to make server side requests.
     // Environment variable API_URL can be used to override baseURL.
     // baseURL: 'http://localhost:8008'
-    baseURL: 'http://localhost:8001'
+    baseURL: process.env.BACKEND_URL
+  },
+
+  server: {
+    port: 3000, // default: 3000
+    host: "0.0.0.0" // default: localhost
   },
 
   toast: {
-    position: 'top-center',
-    iconPack: 'fontawesome',
+    position: "top-center",
+    iconPack: "fontawesome",
     duration: 3000,
     register: [
       {
-        name: 'defaultSuccess',
+        name: "defaultSuccess",
         message: (payload) =>
-          !payload.msg ? 'Operação bem sucedida' : payload.msg,
+          !payload.msg ? "Operação bem sucedida" : payload.msg,
         options: {
-          type: 'success',
-          icon: 'check',
-          theme: 'outline'
+          type: "success",
+          icon: "check",
+          theme: "outline"
         }
       },
       {
-        name: 'defaultError',
+        name: "defaultError",
         message: (payload) =>
-          !payload.msg ? 'Oops.. Erro inesperado' : payload.msg,
+          !payload.msg ? "Oops.. Erro inesperado" : payload.msg,
         options: {
-          type: 'error',
-          icon: 'times',
-          theme: 'outline'
+          type: "error",
+          icon: "times",
+          theme: "outline"
         }
       }
     ]
@@ -111,29 +122,29 @@ module.exports = {
 
       local: {
         endpoints: {
-          login: { url: '/v2/token/', method: 'post', propertyName: 'access', altProperty: 'refresh' },
-          user: { url: '/v2/user/', method: 'get', propertyName: false },
+          login: {url: "/v2/token/", method: "post", propertyName: "access", altProperty: "refresh"},
+          user: {url: "/v2/user/", method: "get", propertyName: false},
           logout: false,
         }
       }
     },
     redirect: {
-      login: '/',
-      logout: '/',
-      home: '/painel'
+      login: "/",
+      logout: "/",
+      home: "/painel"
     },
   },
   build: {
     transpile: [
-      '@nuxtjs/auth',
-      'vee-validate/dist/rules' // <-- TODO Para que serve isso?
+      "@nuxtjs/auth",
+      "vee-validate/dist/rules" // <-- TODO Para que serve isso?
     ],
     /*
     ** You can extend webpack config here
     */
     extend(config, ctx) {
       // Added Line
-      config.devtool = ctx.isClient ? 'eval-source-map' : 'inline-source-map'
+      config.devtool = ctx.isClient ? "eval-source-map" : "inline-source-map";
 
       // Run ESLint on save
       // if (ctx.isDev && ctx.isClient) {
@@ -146,7 +157,7 @@ module.exports = {
       // }
 
     },
-    extractCSS: process.env.NODE_ENV === 'production',
+    extractCSS: process.env.NODE_ENV === "production",
     babel: {
       plugins: [
         [
@@ -159,4 +170,4 @@ module.exports = {
       ]
     }
   }
-}
+};
