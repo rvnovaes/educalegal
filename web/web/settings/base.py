3ff90521029ab87c506acb6e666e5dc57f5798c2
Roberto Novaes <rnovaes@ufmg.br>
2020-08-27 19:11:00 -0300

    slx-279: removida configuracao duplicada do JWT

--- web/web/settings/base.py
+++ web/web/settings/base.py
@@ -176,15 +176,10 @@ DEFAULT_FROM_EMAIL = "sistemas@educalegal.com.br"
 # API Settings
 LOGIN_URL = "/api/v2/login"
 
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
-}
-
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        # "rest_framework.permissions.IsAuthenticated",
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
+        # "rest_framework.permissions.AllowAny",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
