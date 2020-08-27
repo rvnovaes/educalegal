e840cb93a4cd942923759ba3a4401cdcd1a88c33
Roberto Novaes <rnovaes@ufmg.br>
2020-08-27 19:10:27 -0300

    slx-279: removido comentario inutil

--- front/plugins/axios-educa-legal.js
+++ front/plugins/axios-educa-legal.js
@@ -1,7 +1,5 @@
 export default function ({$axios}){
 
-  // $axios.setToken('Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk3MzUwMzk3LCJqdGkiOiI3ZWRhMzFjMzY0Y2E0M2UxYWFlYTZkNTRlNDAwYTEyMSIsInVzZXJfaWQiOjgxfQ.B8TJTRWbqD5TM4mi7r6G9ons4_xh-Ox08SVTm34u5-o', 'Bearer');
-
   $axios.onRequest(config => {
     console.log('Making request to ' + config.url)
     }
