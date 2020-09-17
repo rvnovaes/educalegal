7d1e0fa1d4b11e0ba386f5895128a2652df57d21
iasmini <iasmini.gomes@gmail.com>
2020-09-16 21:39:35 -0300

    removidas msg de log e feita request de criacao de doc

--- api_requests/mayan_requests.py
+++ api_requests/mayan_requests.py
@@ -1,28 +1,39 @@
 import sys
-from requests import Session
-# https://github.com/bustawin/retry-requests
-from retry_requests import retry
-import time
-from requests.exceptions import RequestException
 
+sys.path.append("/opt/educalegal/docassemble/docassemble/brcomeducalegal/data")
 from element_mayan_client import MayanClient
 
 base_url = 'http://ged:8000'
 tenant_ged_token = '483473d37efb328b827c354471be6e082236a2d9'
+
+# base_url = 'https://gedtest.educalegal.com.br'
+# tenant_ged_token = '5bbf160ccb0d02c7da908a7fea5301bdd07f132e'
+
 # iasmini
 # tenant_ged_token = '47f210da48587cb14357e4352d31e0a9c3ae63c0'
 
+mc = MayanClient(base_url, tenant_ged_token)
+
+
+def create_document():
+    filename = '20200916-060329-termo-de-acordo-individual-de-banco-de-horas-mp-927-2020.pdf'
+    document_type = 1
+    label = "aaaa20200916-060329-termo-de-acordo-individual-de-banco-de-horas-mp-927-2020.pdf"
+    language = "por"
+    description = "teste | teste | 2020-05-26"
+
+    # response = mc.document_create(filename, document_type, label, language, description)
+
+    return mc.document_create_message_docid(filename, document_type, label, language, description)
+
+
 if __name__ == '__main__':
-    mc = MayanClient(base_url, tenant_ged_token)
-    # filename = 'lorem-ipsum.pdf'
-    # label = 'lorem_ipsum_teste'
-    # response = mc.document_create(filename, 1, label, 'por', 'Apenas um teste...').json()
+    print(create_document())
+    # document_id = 1958
+    # response = mc.document_read(document_id)
     # print(response)
-    document_id = 1958
-    response = mc.document_read(document_id)
-    print(response)
-    response = mc.document_download(document_id)
-    print(response)
+    # response = mc.document_download(document_id)
+    # print(response)
 
 
 
