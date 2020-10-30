306ef6cb183374fd4b85ffc862c973c50b33f6cf
Roberto Novaes <rnovaes@ufmg.br>
2020-10-30 10:56:04 -0300

    slx-337: acertos para rodar https

--- environments/rebuild.sh
+++ environments/rebuild.sh
@@ -2,5 +2,5 @@
 make stop
 docker system prune -f
 docker rmi -f environments_nginx:latest
-docker volume rm -f environments_certbot-var environments_certbot-etc environments_web-root
+docker volume prune -f
 docker builder prune -a -f
