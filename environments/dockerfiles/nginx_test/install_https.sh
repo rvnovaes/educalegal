e24aaf911bb9ee9bec55128f8ac6ccc7dd34ca9d
Roberto Novaes <rnovaes@ufmg.br>
2020-10-30 16:13:26 -0300

    slx-337: acertos para rodar https

--- environments/dockerfiles/nginx_test/install_https.sh
+++ environments/dockerfiles/nginx_test/install_https.sh
@@ -3,4 +3,4 @@ docker exec nginx certbot certonly --nginx --email sistemas@educalegal.com.br --
 docker cp conf_final/api.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/educalegal.conf nginx:/etc/nginx/conf.d
 docker restart nginx
-crontab -l | { cat; echo "0 0 2-30/2 * * /opt/educalegal/environments/dockerfiles/nginx_test/ssl_renew.sh"; } | crontab -
+source ./add_renew_crontab.sh
