ddbb39c2527efa72946a1b22a13c298bd0a57cde
iasmini <iasmini.gomes@gmail.com>
2020-11-03 18:35:09 -0300

    slx-358: incluido hash para img, fonts e video

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -133,9 +133,12 @@ module.exports = {
       ]
     },
     filenames: {
-      app: "[name].[contenthash].js",
-      chunk: "[name].[contenthash].js",
-      css: "[name].[contenthash].css",
+      app: '[name].[contenthash].js',
+      chunk: '[name].[contenthash].js',
+      css: '[name].[contenthash].css',
+      img: 'img/[contenthash:7].[ext]',
+      font: 'fonts/[contenthash:7].[ext]',
+      video: 'videos/[contenthash:7].[ext]'
     }
   }
 };
