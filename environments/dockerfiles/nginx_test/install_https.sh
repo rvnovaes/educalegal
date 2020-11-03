b3fa4499c3c9125c05c57725b4bc0ca099195ee5
Roberto Novaes <rnovaes@ufmg.br>
2020-11-03 20:04:50 -0300

    slx-337: adicionado arquivo para proxy reverso do docassemble.conf

--- environments/dockerfiles/nginx_test/install_https.sh
+++ environments/dockerfiles/nginx_test/install_https.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker exec nginx certbot certonly --nginx --email sistemas@educalegal.com.br --agree-tos --non-interactive -d apptest.educalegal.com.br -d apitest.educalegal.com.br -d doctest.educalegal.com.br
 docker cp conf_final/api.conf nginx:/etc/nginx/conf.d
+docker cp conf_final/docassemble.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/educalegal.conf nginx:/etc/nginx/conf.d
 docker restart nginx
 source ./add_renew_crontab.sh
