6ce7f99c5b83cb7ed6a5966b60b907ed5356cb4e
Roberto Novaes <rnovaes@ufmg.br>
2020-11-04 15:01:14 -0300

    slx-337: configuracao do docassemble

--- environments/dockerfiles/nginx_test/config_docassemble_internal.sh
+++ environments/dockerfiles/nginx_test/config_docassemble_internal.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-DACONFIGFILE='/usr/share/docassemble/config/config.yml'
-# Edita o arquivo /etc/timezone e troca a primeira linha por America/Sao_Paulo
-sed -i "1 s/^.*$/America/Sao_Paulo" /etc/timezone
+TZF="/etc/timezone"
+DACONFIG="/usr/share/docassemble/config/config.yml"
+# Apaga o arquivo /etc/timezone e cria novo arquivpo troca a primeira linha por America/Sao_Paulo
+rm $TZF
+touch $TZF
+echo "America/Sao_Paulo" >> $TZF
 # Reconfigura a timezone
 dpkg-reconfigure -f noninteractive tzdata
- # Edita o arquivo de configuração do docassemble
-sed -i "s/exitpage: https:\/\/docassemble.org/exitpage: https:\/\/test.educalegal.com.br/" $DACONFIGFILE
-sed -i "s/behind https load balancer: false/behind https load balancer: true/" $DACONFIGFILE
-sed -i "s/language: en/language: pt/" $DACONFIGFILE
-sed -i "s/locale: en_US.utf8/locale: pt_BR.utf8/" $DACONFIGFILE
-sed -i "s/us-words.yml/pt-br-words.yml/" $DACONFIGFILE
+# Edita o arquivo de configuração do docassemble
+sed -i "s/exitpage: https:\/\/docassemble.org/exitpage: https:\/\/test.educalegal.com.br/" $DACONFIG
+sed -i "s/behind https load balancer: false/behind https load balancer: true/" $DACONFIG
+sed -i "s/language: en/language: pt/" $DACONFIG
+sed -i "s/locale: en_US.utf8/locale: pt_BR.utf8/" $DACONFIG
+sed -i "s/us-words.yml/pt-br-words.yml/" $DACONFIG
 # Adiciona as linhas no final do arquivo.
-echo "admin full width: true" >> $DACONFIGFILE
-echo "el environment: test" >> $DACONFIGFILE
-echo "el log to console: true"  >> $DACONFIGFILE
-echo "allow non-idempotent questions: false" >> $DACONFIGFILE
+echo "admin full width: true" >> $DACONFIG
+echo "el environment: test" >> $DACONFIG
+echo "el log to console: true"  >> $DACONFIG
+echo "allow non-idempotent questions: false" >> $DACONFIG
