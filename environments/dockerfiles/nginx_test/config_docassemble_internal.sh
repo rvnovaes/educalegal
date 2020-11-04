ae864f9c2578b440a7353905af6cb145e61af7e6
Roberto Novaes <rnovaes@ufmg.br>
2020-11-04 12:46:49 -0300

    slx-337: configuracao do docassemble

--- environments/dockerfiles/nginx_test/config_docassemble_internal.sh
+++ environments/dockerfiles/nginx_test/config_docassemble_internal.sh
@@ -9,7 +9,8 @@ sed -i "s/exitpage: https:\/\/docassemble.org/exitpage: https:\/\/test.educalega
 sed -i "s/behind https load balancer: false/behind https load balancer: true/" $DACONFIGFILE
 sed -i "s/language: en/language: pt/" $DACONFIGFILE
 sed -i "s/locale: en_US.utf8/locale: pt_BR.utf8/" $DACONFIGFILE
-sed -i "s/  - docassemble.base:data/sources/us-words.yml/  - docassemble.base:data/sources/pt-br-words.yml/" $DACONFIGFILE
+sed -i "s/us-words.yml/pt-br-words.yml/" $DACONFIGFILE
+# Adiciona as linhas no final do arquivo.
 echo "admin full width: true" >> $DACONFIGFILE
 echo "el environment: test" >> $DACONFIGFILE
 echo "el log to console: true"  >> $DACONFIGFILE
