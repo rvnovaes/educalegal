5a4b7d657d18f26e7b6f23bd016de7f5a35c1060
Roberto Novaes <rnovaes@ufmg.br>
2020-11-04 11:16:59 -0300

    slx-337: adicionado menu de testes

--- environments/dockerfiles/nginx_test/install_https.sh
+++ environments/dockerfiles/nginx_test/install_https.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker exec nginx certbot certonly --nginx --email sistemas@educalegal.com.br --agree-tos --non-interactive -d apptest.educalegal.com.br -d apitest.educalegal.com.br -d doctest.educalegal.com.br
+docker exec nginx certbot certonly --nginx --email sistemas@educalegal.com.br --agree-tos --non-interactive -d apptest.educalegal.com.br -d apitest.educalegal.com.br -d doctest.educalegal.com.br -d test.educalegal.com.br
 docker cp conf_final/api.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/docassemble.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/educalegal.conf nginx:/etc/nginx/conf.d
