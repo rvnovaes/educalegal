ddf3e377c0efdf864508aec93790170e6a089d5e
Roberto Novaes <rnovaes@ufmg.br>
2020-11-05 18:03:04 -0300

    slx-337: correcao de typo no install_https.sh

--- environments/dockerfiles/nginx_test/install_https.sh
+++ environments/dockerfiles/nginx_test/install_https.sh
@@ -2,7 +2,7 @@
 docker exec nginx certbot certonly --nginx --email sistemas@educalegal.com.br --agree-tos --non-interactive -d apptest.educalegal.com.br -d apitest.educalegal.com.br -d doctest.educalegal.com.br -d test.educalegal.com.br -d apiflowertest.educalegal.com.br -d apimongoexpresstest.educalegal.com.br -d apirabbitmqtest.educalegal.com.br
 docker cp conf_final/api.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/docassemble.conf nginx:/etc/nginx/conf.d
-docker cp conf_final/educalegal.conf nginx:/etc/nginx/conf.dO
+docker cp conf_final/educalegal.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/menu.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/apirabbitmq.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/apimongoexpress.conf nginx:/etc/nginx/conf.d
