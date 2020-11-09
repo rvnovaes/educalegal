293dbc24786b75e1faad250abd211170aeda3f94
iasmini <iasmini.gomes@gmail.com>
2020-11-09 18:47:31 -0300

    slx-337: retirada criacao de certificado para docassemble de producao

--- environments/dockerfiles/nginx_prod/install_https.sh
+++ environments/dockerfiles/nginx_prod/install_https.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-docker exec nginx certbot certonly --nginx --email sistemas@educalegal.com.br --agree-tos --non-interactive -d app.educalegal.com.br -d api.educalegal.com.br -d generation.educalegal.com.br -d production.educalegal.com.br -d apiflower.educalegal.com.br -d apimongoexpress.educalegal.com.br -d apirabbitmq.educalegal.com.br
+#docker exec nginx certbot certonly --nginx --email sistemas@educalegal.com.br --agree-tos --non-interactive --expand -d app.educalegal.com.br -d api.educalegal.com.br -d generation.educalegal.com.br -d production.educalegal.com.br -d apiflower.educalegal.com.br -d apimongoexpress.educalegal.com.br -d apirabbitmq.educalegal.com.br
+docker exec nginx certbot certonly --nginx --email sistemas@educalegal.com.br --agree-tos --non-interactive -d app.educalegal.com.br -d api.educalegal.com.br -d production.educalegal.com.br -d apiflower.educalegal.com.br -d apimongoexpress.educalegal.com.br -d apirabbitmq.educalegal.com.br
 docker cp conf_final/api.conf nginx:/etc/nginx/conf.d
-docker cp conf_final/docassemble.conf nginx:/etc/nginx/conf.d
+#docker cp conf_final/docassemble.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/educalegal.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/menu.conf nginx:/etc/nginx/conf.d
 docker cp conf_final/apirabbitmq.conf nginx:/etc/nginx/conf.d
