6991810afb5d4871ee56f08b16e356c42cccd004
Iasmini Gomes <iasmini.gomes@e-deploy.com.br>
2021-03-21 14:57:25 -0300

    alterado ip do servidor de teste do app

--- web/web/settings/test.py
+++ web/web/settings/test.py
@@ -2,7 +2,7 @@ from .base import *
 
 from django.contrib.messages import constants as message_constants
 
-ALLOWED_HOSTS = ["apptest.educalegal.com.br", "apitest.educalegal.com.br", "104.248.1.2"]
+ALLOWED_HOSTS = ["apptest.educalegal.com.br", "apitest.educalegal.com.br", "167.172.248.183"]
 
 DEBUG = True
 MESSAGE_LEVEL = message_constants.INFO
