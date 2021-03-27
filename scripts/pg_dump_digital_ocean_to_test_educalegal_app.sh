766074bfee11cae5978cf24e297943231089dd32
Iasmini Gomes <iasmini.gomes@e-deploy.com.br>
2021-03-27 14:17:37 -0300

    alterada url do space de teste

--- scripts/pg_dump_digital_ocean_to_test_educalegal_app.sh
+++ scripts/pg_dump_digital_ocean_to_test_educalegal_app.sh
@@ -10,7 +10,7 @@ pg_dump --host=educa-legal-producao-db-postgresql-do-user-106912-0.db.ondigitalo
   --no-password \
   --verbose \
   --clean > ${FILENAME}
-PGPASSWORD=lvr0emmvuc8241ez dropdb -U doadmin -h educa-legal-development-db-postgresql-do-user-106912-0.a.db.ondigitalocean.com -p 25060 --echo educa-legal-test
-PGPASSWORD=lvr0emmvuc8241ez createdb -U doadmin -h educa-legal-development-db-postgresql-do-user-106912-0.a.db.ondigitalocean.com -p 25060 --echo educa-legal-test
-PGPASSWORD=lvr0emmvuc8241ez psql -U doadmin -d educa-legal-test -h educa-legal-development-db-postgresql-do-user-106912-0.a.db.ondigitalocean.com -e -p 25060 < ${FILENAME}
+PGPASSWORD=wxk5gsxfqj8jnru2 dropdb -U doadmin -h educa-legal-development-db-postgresql-do-user-8910843-0.b.db.ondigitalocean.com -p 25060 --echo educa-legal-test
+PGPASSWORD=wxk5gsxfqj8jnru2 createdb -U doadmin -h educa-legal-development-db-postgresql-do-user-8910843-0.b.db.ondigitalocean.com -p 25060 --echo educa-legal-test
+PGPASSWORD=wxk5gsxfqj8jnru2 psql -U doadmin -d educa-legal-test -h educa-legal-development-db-postgresql-do-user-8910843-0.b.db.ondigitalocean.com -e -p 25060 < /opt/educalegal/scripts/educa-legal-app-20210321-113000.sql
 rm ${FILENAME}
